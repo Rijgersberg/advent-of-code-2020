612280d3602141d7f4fcad075f16bd6e166ec795
Edwin Rijgersberg <edwin@edwinrijgersberg.nl>
2020-12-01 09:20:15 +0100

    day 1

--- 1.py
+++ 1.py
@@ -1,3 +1,15 @@
+from itertools import combinations
 
 
+with open('input/1.txt') as f:
+    expenses = [int(l) for l in f.readlines()]
 
+# 1-1
+for e1, e2 in combinations(expenses, 2):
+    if e1 + e2 == 2020:
+        print(e1, e2, e1 * e2)
+
+# 1-2
+for e1, e2, e3 in combinations(expenses, 3):
+    if e1 + e2 + e3 == 2020:
+        print(e1, e2, e3, e1 * e2 * e3)
