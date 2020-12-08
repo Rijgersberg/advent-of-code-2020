e68dad1a5e20535bc415249502f6281b43154868
Edwin Rijgersberg <edwin@edwinrijgersberg.nl>
2020-12-08 08:03:02 +0100

    Functies bestaan ook nog

--- 8.py
+++ 8.py
@@ -1,9 +1,3 @@
-from collections import defaultdict, Counter, deque
-from dataclasses import dataclass
-import heapq
-from itertools import combinations, combinations_with_replacement, permutations, product
-import re
-
 import copy
 
 from aoc import get_input
@@ -11,46 +5,15 @@ from aoc import get_input
 instructions = get_input(day=8)
 
 
-# 8-1
-i = 0
-acc = 0
-executed = set()
-while i not in executed:
-    executed.add(i)
-
-    op, value = instructions[i].split()
-    value = int(value)
-
-    if op == 'nop':
-        i += 1
-    elif op == 'acc':
-        acc += value
-        i += 1
-    elif op == 'jmp':
-        i += value
-    else:
-        raise ValueError
-
-print(acc)
+def parse(instructions):
+    parsed = []
+    for instruction in instructions:
+        op, value = instruction.split()
+        parsed.append([op, int(value)])
+    return parsed
 
 
-# 8-2
-def perturbed_instructions(original):
-    original = [line.split() for line in original]
-
-    for i, line in enumerate(original):
-        if line[0] == 'nop':
-            new = copy.deepcopy(original)
-            new[i][0] = 'jmp'
-            yield new
-        elif line[0] == 'jmp':
-            new = copy.deepcopy(original)
-            new[i][0] = 'nop'
-            yield new
-
-this_one_right_here_officer = False
-original = instructions
-for instructions in perturbed_instructions(original):
+def run(instructions):
     i = 0
     acc = 0
     executed = set()
@@ -58,7 +21,6 @@ for instructions in perturbed_instructions(original):
         executed.add(i)
 
         op, value = instructions[i]
-        value = int(value)
 
         if op == 'nop':
             i += 1
@@ -70,12 +32,30 @@ for instructions in perturbed_instructions(original):
         else:
             raise ValueError
 
+    return i, acc
+
+
+# 8-1
+i, acc = run(parse(instructions))
+print(acc)
+
+
+# 8-2
+def perturbed_instructions(original):
+    for i, line in enumerate(original):
+        if line[0] == 'nop':
+            new = copy.deepcopy(original)
+            new[i][0] = 'jmp'
+            yield new
+        elif line[0] == 'jmp':
+            new = copy.deepcopy(original)
+            new[i][0] = 'nop'
+            yield new
+
+
+for instructions in perturbed_instructions(parse(instructions)):
+    i, acc = run(instructions)
+
     if i == len(instructions):
-        this_one_right_here_officer = True
+        print(acc)
         break
-
-if this_one_right_here_officer:
-    print(acc)
-
-
-
