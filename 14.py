72b66eaf9fee152b46d67a47e9b69a7a2c1a7493
Edwin Rijgersberg <edwin@edwinrijgersberg.nl>
2020-12-14 18:56:23 +0100

    Newlines are at a premium these days

--- 14.py
+++ 14.py
@@ -1,7 +1,4 @@
-from collections import defaultdict, Counter, deque
-from dataclasses import dataclass
-import heapq
-from itertools import combinations, combinations_with_replacement, permutations, product
+from itertools import product, count
 import re
 
 from aoc import get_input
@@ -14,45 +11,29 @@ mask = {}
 mem = {}
 for instruction in instructions:
     if instruction.startswith('mask'):
-        mask_str = re.fullmatch(r'mask = ([01X]+)', instruction).groups()[0]
+        _, mask = instruction.split(' = ')
     else:
         index, value = re.fullmatch(r'mem\[(\d+)\] = (\d+)', instruction).groups()
 
-        bit_list = list("{0:b}".format(int(value)).zfill(36))
-        for i, v in enumerate(mask_str):
-            if v != 'X':
-                bit_list[i] = v
-        mem[int(index)] = int(''.join(bit_list), 2)
+        mem[int(index)] = int(''.join(
+            c if mask[i] == 'X' else mask[i]
+            for i, c in enumerate(f'{int(value):036b}')),
+            2)
 print(sum(mem.values()))
 
 # 14-2
 mem = {}
 for instruction in instructions:
     if instruction.startswith('mask'):
-
-        mask_str = re.fullmatch(r'mask = ([01X]+)', instruction).groups()[0]
-        mask_idc = [m.start() for m in re.finditer(r'X', mask_str)]
+        _, mask = instruction.split(' = ')
     else:
         index, value = re.fullmatch(r'mem\[(\d+)\] = (\d+)', instruction).groups()
 
-        bit_str = "{0:b}".format(int(index)).zfill(36)
-        if len(mask_idc) <= 4:
-            print()
-            print(bit_str)
-            print(mask_str)
-        for filler in product(('0', '1'), repeat=len(mask_idc)):
-            bit_list = list(bit_str)
-
-            filler_idx = 0
-            for i, v in enumerate(mask_str):
-                if v == '1':
-                    bit_list[i] = v
-                elif v == 'X':
-                    bit_list[i] = filler[filler_idx]
-                    filler_idx += 1
-            med_idx = int(''.join(bit_list), 2)
-            if len(mask_idc) <= 4:
-                print(''.join(bit_list))
-
+        for filler in product(('0', '1'), repeat=mask.count('X')):
+            counter = count()
+            med_idx = int(''.join(
+                mask[i] if mask[i] == '1' else filler[next(counter)] if mask[i] == 'X' else c
+                for i, c in enumerate(f'{int(index):036b}')),
+                2)
             mem[med_idx] = int(value)
 print(sum(mem.values()))
