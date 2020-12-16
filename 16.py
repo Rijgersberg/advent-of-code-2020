d55a0be73ea1c33a1204fc9eaa721fec4ce49256
Edwin Rijgersberg <edwin@edwinrijgersberg.nl>
2020-12-16 19:45:43 +0100

    Refactor 16-1 with for/else

--- 16.py
+++ 16.py
@@ -3,33 +3,30 @@ import re
 
 from aoc import get_input
 
+
 ipt = get_input(day=16)
 
+rules_str = ipt[:20]
+my_ticket = [int(n) for n in ipt[22].split(',')]
+nearby_tickets = [tuple(map(int, line.split(','))) for line in ipt[25:]]
+N = len(my_ticket)
+
 rules = {}
-for line in ipt[:20]:
+for line in rules_str:
     name, l1, u1, l2, u2 = re.fullmatch(r'([a-z ]+): (\d+)-(\d+) or (\d+)-(\d+)', line).groups()
     l1, u1, l2, u2 = map(int, (l1, u1, l2, u2))
     rules[name] = range(l1, u1+1), range(l2, u2+1)
 
-your_ticket = [int(n) for n in ipt[22].split(',')]
-nearby_tickets = [tuple(map(int, line.split(','))) for line in ipt[25:]]
-N = len(your_ticket)
-
-
 # 16-1
 error_rate = 0
-invalid_tickets = set()
+valid_tickets = set()
 for ticket in nearby_tickets:
     for num in ticket:
-        is_valid = False
-        for range1, range2 in rules.values():
-            if num in range1 or num in range2:
-                is_valid = True
-                break
-        if not is_valid:
+        if not any(num in r1 or num in r2 for r1, r2 in rules.values()):
             error_rate += num
-            invalid_tickets |= {ticket}
-valid_tickets = set(nearby_tickets) - invalid_tickets
+            break
+    else:
+        valid_tickets |= {ticket}
 print(error_rate)
 
 # 16-2
@@ -52,4 +49,4 @@ while unfixed_posses:
     final_posses[shortest_rule] = list(shortest_pos)[0]
     unfixed_posses = {rule: posses - shortest_pos for rule, posses in unfixed_posses.items() if rule != shortest_rule}
 
-print(math.prod(your_ticket[pos] for rule, pos in final_posses.items() if rule.startswith('departure')))
+print(math.prod(my_ticket[pos] for rule, pos in final_posses.items() if rule.startswith('departure')))
