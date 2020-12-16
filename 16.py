3be895dfbf1f934630204dc49268a1c381be24b0
Edwin Rijgersberg <edwin@edwinrijgersberg.nl>
2020-12-16 07:03:36 +0100

    Cleanup

--- 16.py
+++ 16.py
@@ -1,8 +1,4 @@
 import math
-from collections import defaultdict, Counter, deque
-from dataclasses import dataclass
-import heapq
-from itertools import combinations, combinations_with_replacement, permutations, product
 import re
 
 from aoc import get_input
@@ -11,18 +7,20 @@ ipt = get_input(day=16)
 
 rules = {}
 for line in ipt[:20]:
-    name, l1, u1, l2, u2 = re.fullmatch('([a-z ]+): (\d+)-(\d+) or (\d+)-(\d+)', line).groups()
+    name, l1, u1, l2, u2 = re.fullmatch(r'([a-z ]+): (\d+)-(\d+) or (\d+)-(\d+)', line).groups()
     l1, u1, l2, u2 = map(int, (l1, u1, l2, u2))
     rules[name] = range(l1, u1+1), range(l2, u2+1)
 
-nearby_tickets = ipt[25:]
+your_ticket = [int(n) for n in ipt[22].split(',')]
+nearby_tickets = [tuple(map(int, line.split(','))) for line in ipt[25:]]
+N = len(your_ticket)
+
 
 # 16-1
 error_rate = 0
 invalid_tickets = set()
 for ticket in nearby_tickets:
-    for num in ticket.split(','):
-        num = int(num)
+    for num in ticket:
         is_valid = False
         for range1, range2 in rules.values():
             if num in range1 or num in range2:
@@ -37,10 +35,10 @@ print(error_rate)
 # 16-2
 rules_posses = {}
 for rule, (r1, r2) in rules.items():
-    valid_posses = set(range(20))
-    for pos in range(20):
+    valid_posses = set(range(N))
+    for pos in range(N):
         for ticket in valid_tickets:
-            num = ticket.split(',')[pos]
+            num = ticket[pos]
             if not (int(num) in r1 or int(num) in r2):
                 valid_posses -= {pos}
     rules_posses[rule] = valid_posses
@@ -52,19 +50,6 @@ while unfixed_posses:
     assert len(shortest_pos) == 1
 
     final_posses[shortest_rule] = list(shortest_pos)[0]
-
-    # for rule in unfixed_posses:
-    #     unfixed_posses[rule] -= shortest_pos
-    unfixed_posses = {rule: posses - shortest_pos for rule, posses in unfixed_posses.items()}
-
-    del unfixed_posses[shortest_rule]
-print(final_posses)
-
-your_ticket = [int(n) for n in ipt[22].split(',')]
-print(your_ticket)
+    unfixed_posses = {rule: posses - shortest_pos for rule, posses in unfixed_posses.items() if rule != shortest_rule}
 
 print(math.prod(your_ticket[pos] for rule, pos in final_posses.items() if rule.startswith('departure')))
-
-
-
-
