66bfa81df1ee0983081a9b25a702eebbdcdb239f
Edwin Rijgersberg <edwin@edwinrijgersberg.nl>
2020-12-19 22:30:49 +0100

    Cleanup

--- 19.py
+++ 19.py
@@ -16,10 +16,10 @@ def pattern(n):
 
     if PART == 2:
         if n == '8':
-            return f"({pattern('42')})+"
+            return '(' + pattern('42') + ')+'
         elif n == '11':
             # 42 31 | 42 42 31 31 | 42 42 42 31 31 31 | ...
-            enough = 10  # wow...
+            enough = 6  # wow
             return '(' + '|'.join(f"{pattern('42')}{{{i}}}{pattern('31')}{{{i}}}" for i in range(1, enough)) + ')'
 
     if '"a"' in rule:
@@ -27,32 +27,35 @@ def pattern(n):
     elif '"b"' in rule:
         return 'b'
     elif '|' in rule:
-        p1, p2 = rule.split('|')
-        p1, p2 = p1.strip(), p2.strip()
-        return f'({"".join(map(pattern, p1.split(" ")))}|{"".join(map(pattern, p2.split(" ")))})'
+        p1, p2 = rule.split(' | ')
+        return '(' + ''.join(pattern(c) for c in p1.split(' ')) + '|' + ''.join(pattern(c) for c in p2.split(' ')) + ')'
     else:
-        return ''.join(map(pattern, rule.split(' ')))
+        return ''.join(pattern(c) for c in rule.split(' '))
+
+
+def n_valid(messages, start):
+    return sum(re.fullmatch(pattern(start), message) is not None for message in messages)
 
 
 # test-1
 PART = 1
 with open('input/19-1-test.txt') as f:
     rules, messages = parse_input(f.read())
-assert sum(re.fullmatch(pattern('0'), message) is not None for message in messages) == 2
+assert n_valid(messages, start='0') == 2
 
 # test-2
 PART = 2
 with open('input/19-2-test.txt') as f:
     rules, messages = parse_input(f.read())
-assert sum(re.fullmatch(pattern('0'), message) is not None for message in messages) == 12
+assert n_valid(messages, start='0') == 12
 
 # 19
 rules, messages = parse_input(get_input(day=19, as_list=False))
 
 # 19-1
 PART = 1
-print(sum(re.fullmatch(pattern('0'), message) is not None for message in messages))
+print(n_valid(messages, start='0'))
 
 # 19-2
 PART = 2
-print(sum(re.fullmatch(pattern('0'), message) is not None for message in messages))
+print(n_valid(messages, start='0'))
