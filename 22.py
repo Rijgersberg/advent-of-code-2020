d7f371bac66d9781e5c0c4fa52ccaccca8d4b3c9
Edwin Rijgersberg <edwin@edwinrijgersberg.nl>
2020-12-22 09:19:59 +0100

    Cleanup

--- 22.py
+++ 22.py
@@ -1,92 +1,43 @@
-from collections import deque
-
 from aoc import get_input
 
 
 def parse(input_):
-    deck1 = deque()
-    deck2 = deque()
-    for line in input_:
-        if line == 'Player 1:':
-            deck = deck1
-        elif line == 'Player 2:':
-            deck = deck2
-        elif line.isnumeric():
-            deck.append(int(line))
-    return deck1, deck2
+    p1_str, p2_str = input_.split('\n\n')
+    return tuple(int(l) for l in p1_str.splitlines()[1:]), \
+           tuple(int(l) for l in p2_str.splitlines()[1:])
 
 
-def play(deck1, deck2):
-    round = 0
+def play(deck1, deck2, recursive=False):
+    seen = set()
     while deck1 and deck2:
-        round += 1
-        c1, c2 = deck1.popleft(), deck2.popleft()
+        if recursive:
+            if (deck1, deck2) in seen:
+                return 1, (deck1, deck2)
+            else:
+                seen.add((deck1, deck2))
 
-        if c1 > c2:
-            deck1.append(c1)
-            deck1.append(c2)
-        elif c2 > c1:
-            deck2.append(c2)
-            deck2.append(c1)
+        c1, deck1 = deck1[0], deck1[1:]
+        c2, deck2 = deck2[0], deck2[1:]
+
+        if recursive and len(deck1) >= c1 and len(deck2) >= c2:
+            winner, _ = play(deck1[:c1], deck2[:c2], recursive)
         else:
-            raise ValueError
-    return deck1 if deck1 else deck2
+            winner = 1 if c1 > c2 else 2
+
+        if winner == 1:
+            deck1 = (*deck1, c1, c2)
+        elif winner == 2:
+            deck2 = (*deck2, c2, c1)
+
+    return 1 if deck1 else 2, deck1 if deck1 else deck2
 
 
 def score(deck):
     return sum(i * c for i, c in enumerate(reversed(deck), 1))
 
 
-# 22-1 test
-with open('input/22-1-test.txt') as f:
-    assert score(play(*parse(f.read().splitlines()))) == 306
-
 # 22-1
-print(score(play(*parse(get_input(day=22)))))
-
-
-# 22-2
-def freeze(deck1, deck2):
-    return tuple(deck1), tuple(deck2)
-
-
-def play_recursive(deck1, deck2):
-    round = 0
-    seen = set()
-    while deck1 and deck2:
-        round += 1
-
-        frozen_decks = freeze(deck1, deck2)
-        if frozen_decks in seen:
-            return 1, frozen_decks
-        else:
-            seen.add(frozen_decks)
-
-        c1, c2 = deck1.popleft(), deck2.popleft()
-
-        if len(deck1) >= c1 and len(deck2) >= c2:
-            # To play a sub-game of Recursive Combat, each player creates a new deck by making a copy of the next cards
-            # in their deck (the quantity of cards copied is equal to the number on the card they drew to trigger the sub-game).
-            new_deck_1 = deque(list(deck1)[:c1])
-            new_deck_2 = deque(list(deck2)[:c2])
-            winner, _ = play_recursive(new_deck_1, new_deck_2)
-        else:
-            winner = 1 if c1 > c2 else 2
-
-        if winner == 1:
-            deck1.append(c1)
-            deck1.append(c2)
-        elif winner == 2:
-            deck2.append(c2)
-            deck2.append(c1)
-        else:
-            raise ValueError
-    return 1 if deck1 else 2, deck1 if deck1 else deck2
-
-
-# 22-2 test
-with open('input/22-1-test.txt') as f:
-    assert score(play_recursive(*parse(f.read().splitlines()))[1]) == 291
+print(score(play(*parse(get_input(day=22, as_list=False)))[1]))
 
 # 22-2
-print(score(play_recursive(*parse(get_input(day=22)))[1]))
+print(score(play(*parse(get_input(day=22, as_list=False)), recursive=True)[1]))
