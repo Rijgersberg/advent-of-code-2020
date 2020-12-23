a43f37da2c73e4cd0a5d00d9d89058f3103a4935
Edwin Rijgersberg <edwin@edwinrijgersberg.nl>
2020-12-23 09:51:33 +0100

    Solve Day 23-2

--- 23.py
+++ 23.py
@@ -38,7 +38,7 @@ def play(cups, moves):
         last = cups.popleft()
         cups.append(last)
 
-        print(f'{move=}, {destination_cup=}, {list(cups)[:100]}, {list(cups)[-100:]}')
+        # print(f'{move=}, {destination_cup=}, {list(cups)[:100]}, {list(cups)[-100:]}')
 
     return list(cups)
 
@@ -48,22 +48,74 @@ def reorder(cups, start_at=1):
     return [start_at] + cups[start_idx + 1:] + cups[:start_idx]
 
 
+def linked_list(cups):
+    print(f'len of cups is {len(cups)}')
+    next_ = [None] * (len(cups) + 1)  # mimic one-based-indexing
+    print(f'len of next_ is {len(next_)}')
+    for i in range(len(cups) - 1):
+        next_[cups[i]] = cups[i+1]
+    next_[cups[-1]] = cups[0]
+
+    return next_
+
+def play2(next_, start_cup, moves):
+    max_value = len(next_) - 1
+
+    current_cup = start_cup
+    for move in range(1, moves + 1):
+        c1 = next_[current_cup]
+        c2 = next_[c1]
+        c3 = next_[c2]
+
+        destination_cup = decrease(current_cup, 1, max_value)
+        while destination_cup in (c1, c2, c3):
+            destination_cup = decrease(destination_cup, 1, max_value)
+
+        next_[current_cup] = next_[c3]
+
+        old_right_of_dest = next_[destination_cup]
+        next_[destination_cup] = c1
+        # next of c1 and c2 stay the same
+        next_[c3] = old_right_of_dest
+
+        current_cup = next_[current_cup]
+
+    return next_
+
+def linked_list_to_sequence(next_, start_at=1):
+    cups = [start_at]
+    prev = start_at
+    for i in range(len(next_) - 2):
+        if prev >= len(next_):
+            print(i, prev, len(next_))
+        prev = next_[prev]
+        cups.append(prev)
+    return cups
+
+
 # 23-1
-test = deque([int(c) for c in '389125467'])
-clockwize = deque([int(c) for c in '643719258'])
+test = [int(c) for c in '389125467']
+clockwize = [int(c) for c in '643719258']
 
-assert play(deepcopy(test), 10) == [int(c) for c in '837419265']
-assert reorder(play(deepcopy(test), 100))[1:] == [int(c) for c in '67384529']
+assert linked_list([int(c) for c in '389125467']) == [None, 2, 5, 8, 6, 4, 7, 3, 9, 1]
+assert linked_list_to_sequence(play2(linked_list(deepcopy(test)), start_cup=test[0], moves=10), start_at=8) == [int(c) for c in '837419265']
+assert linked_list_to_sequence(play2(linked_list(deepcopy(test)), start_cup=test[0], moves=100), start_at=1)[1:] == [int(c) for c in '67384529']
 
-print(''.join(str(c) for c in reorder(play(clockwize, 100))[1:]))
+print(''.join(str(c) for c in linked_list_to_sequence(play2(linked_list(deepcopy(clockwize)), start_cup=clockwize[0], moves=100), start_at=1)[1:]))
 
 # 23-2
 print('=' * 80)
-clockwize = deque([int(c) for c in '643719258'] + list(range(10, 1_000_000+1)))
+
+clockwize = [int(c) for c in '643719258'] + list(range(10, 1_000_000+1))
 
 t0 = time.time()
-moves = 100
-result = play(clockwize, moves)
+moves = 10_000_000
+input_2 = linked_list(deepcopy(clockwize))
+print(f'len of input_2 is {len(input_2)}')
+result = play2(input_2, start_cup=clockwize[0], moves=moves)
+print(f'len of results is {len(result)}')
 t = time.time() - t0
-print(f'{t} seconds, {moves / t} per second, {moves / t * 10_000_000 / 60 / 60 / 24} days for 10 million moves')
+print(f'{t} seconds, {moves / t} per second, {t/moves * 10_000_000 / 60} minutes for 10 million moves')
+c1, c2 = linked_list_to_sequence(result, start_at=1)[1:3]
 
+print(f'{c1=}, {c2=}, {c1 * c2}')
