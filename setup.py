b6e4e164411342c6631b0a74dbe68c94bca0ec26
Edwin Rijgersberg <edwin@edwinrijgersberg.nl>
2020-12-23 20:39:07 +0100

    Cleanup repo

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from Cython.Build import cythonize
 
 setup(
-    ext_modules = cythonize("d15.pyx",
+    ext_modules = cythonize("day15.pyx",
                             compiler_directives={'language_level' : "3"},
                             annotate=True,)
 )
